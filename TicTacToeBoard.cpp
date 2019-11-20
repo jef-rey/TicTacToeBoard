c40ae590b0c1bfd02a281e5d3e80764022c71fe4
Jeff Anderson <j3ff.anderson@gmail.com>
2019-11-20 15:41:01 -0800

    correcting errors in TTTB.cpp

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -2,7 +2,7 @@
 /**
  * Class for representing a 3x3 Tic-Tac-Toe game board, using the Piece enum
  * to represent the spaces on the board.
-**/
+ **/
 
 //Constructor sets an empty board and specifies it is X's turn first
 TicTacToeBoard::TicTacToeBoard()
@@ -16,7 +16,7 @@ TicTacToeBoard::TicTacToeBoard()
 /**
  * Switches turn member variable to represent whether it's X's or O's turn
  * and returns whose turn it is
-**/
+ **/
 Piece TicTacToeBoard::toggleTurn()
 {
   if (turn == X){
@@ -38,11 +38,11 @@ Piece TicTacToeBoard::toggleTurn()
  * Out of bounds coordinates return the Piece Invalid value. When the game
  * is over, no more pieces can be placed so attempting to place a piece
  * should neither change the board nor change whose turn it is.
-**/ 
+ **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
   if (board[row][column] == Blank){
-    placePiece = turn;
+    board[row][column] = turn;
     return turn;
     toggleTurn();
   }
@@ -54,7 +54,7 @@ Piece TicTacToeBoard::placePiece(int row, int column)
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
   if (board[row][column] == X){
@@ -64,14 +64,14 @@ Piece TicTacToeBoard::getPiece(int row, int column)
   }else if (board[row][column] == Blank){
     return Blank;
   }else{
-  return Invalid;
+    return Invalid;
   }
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won.
-**/
+ **/
 Piece TicTacToeBoard::getWinner()
 {
   for (int i = 0; i < BOARDSIZE; i++){
@@ -81,5 +81,6 @@ Piece TicTacToeBoard::getWinner()
       }else if(BOARDSIZE[i][j] == O){
         return O;
       }
-  return Invalid;
-}
+    }
+    return Invalid;
+  }
