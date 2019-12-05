d72820a672f6b9c0669e96a39378c140e79d0009
Jeff Anderson <j3ff.anderson@gmail.com>
2019-12-04 23:04:37 -0800

    fixing x winning col

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -82,11 +82,11 @@ TEST(TicTacToeBoardTest, testGetWinnerX_diag){
 
 TEST(TicTacToeBoardTest, testGetWinnerX_col){
   TicTacToeBoard test;
-  test.placePiece(0,0); // x
-  test.placePiece(1,1); // o
   test.placePiece(1,0); // x
-  test.placePiece(0,1); // o
-  test.placePiece(0,2); // x <-- win
+  test.placePiece(2,1); // o
+  test.placePiece(2,0); // x
+  test.placePiece(2,2); // o
+  test.placePiece(0,0); // x <-- win
   ASSERT_EQ(test.getWinner(), X);
   /*
    *        0       1       2
